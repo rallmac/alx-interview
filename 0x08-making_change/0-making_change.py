21a94b0d30dcf463ed2f1922d13db41b4a4ea7d9
rallmac <tobijahekperikpe@gmail.com>
2024-11-26 15:57:31 +0100

    Optimizing code for efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,11 +9,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    coins.sort(reverse=True)
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
         for amount in range(coin, total + 1):
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+        if dp[total] != float('inf'):
+            break
 
     return dp[total] if dp[total] != float('inf') else -1
